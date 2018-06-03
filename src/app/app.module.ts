30bdc7ba200a76e4dd3e26661ac665f10d049639
wanjiiru <liz240@live.com>
2018-06-03 19:03:52 +0300

    add uppercase pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RouterModule } from '@angular/router';
 import { SearchFormComponent } from './search-form/search-form.component';
 import {FormsModule} from '@angular/forms';
 import {SearchRequestService} from './search-request.service';
+import { UpperCasePipe } from './upper-case.pipe';
 
 
 
@@ -27,7 +28,8 @@ import {SearchRequestService} from './search-request.service';
     NavbarComponent,
     SearchComponent,
     RepositoriesComponent,
-    SearchFormComponent
+    SearchFormComponent,
+    UpperCasePipe
   ],
   imports: [
     BrowserModule,
